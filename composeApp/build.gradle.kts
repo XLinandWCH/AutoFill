d5946b7bdd3150ac5892b791fa864875dba5964f
XLin <xlinandwch@outlook.com>
2026-06-03 20:44:31 +0800

    版本:0.3.9 说明:优化了一些无效代码:HomeSetting.kt

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -80,7 +80,7 @@ compose.desktop {
             // 在这里加上 TargetFormat.Zip
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "AutoFill"
-            packageVersion = "1.0.3"
+            packageVersion = "1.0.4"
 
             // --- 在这里配置图标 ---
 
